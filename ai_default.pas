329b3f4150632b659b1eb72de296d01074405653
vodchella <twister.kz@gmail.com>
2016-01-14 18:52:20 +0600

    Научил движок выуживать информацию о side-соединениях. Метод TAIDefault.GetSideConnectionInfo() возвращает структуру TSideConnectionInformation

--- ai_default.pas
+++ ai_default.pas
@@ -12,7 +12,7 @@ type
   end;
   TRelativePoint = TPointRecord;
 
-  TTwoBridgeConnectionDirection = (TBCDTopLeft, TBCDTop, TBCDTopRight, TBCDBottomRight, TBCDBottom, TBCDBottomLeft);
+  TTwoBridgeConnectionDirection = (CDTopLeft, CDTop, CDTopRight, CDBottomRight, CDBottom, CDBottomLeft);
   TTwoBridgeConnectionState = (TBCSUnknown, TBCSInvalid, TBCSRealized, TBCSNotRealized, TBCSSafe, TBCSDangerous, TBCSFree);
   TTwoBridgeConnectionCarrier = array [0..1] of TRelativePoint;
   TTwoBridgeConnectionInformation = packed record
@@ -21,18 +21,32 @@ type
     point:           TPointRecord;
     PriorityCell:    TCell;
     carrier:         TTwoBridgeConnectionCarrier;
-    BoardConnection: boolean;
+    SideConnection:  boolean;
+  end;
+
+  TSideConnectionDirection = TTwoBridgeConnectionDirection;
+  TSideConnectionCarrier = TTwoBridgeConnectionCarrier;
+  TSideConnectionState = (SCSUnknown, SCSInvalid, SCSRealized, SCSSafe, SCSDangerous, SCSFree);
+  TSideConnectionInformation = packed record
+    direction:       TSideConnectionDirection;
+    state:           TSideConnectionState;
+    PriorityCell:    TCell;
+    carrier:         TSideConnectionCarrier;
   end;
 
   TAIDefault = class(TInterfacedObject, IArtificialIntelligence)
     strict private
       Fboard: TGameBoard;
     private
+      function  GetOppositePlayer(player: TPlayer): TPlayer;
       function  GetFirstMove(BoardSide: shortint): TPoint;
       function  GetPointByRelative(X, Y: shortint; var pt: TRelativePoint): TPointRecord;
       function  GetTwoBridgeConnectionPoint(X, Y: shortint; dir: TTwoBridgeConnectionDirection): TPointRecord;
       function  GetTwoBridgeConnectionCarrier(X, Y: shortint; dir: TTwoBridgeConnectionDirection): TTwoBridgeConnectionCarrier;
       function  GetTwoBridgeConnectionInfo(X, Y: shortint; dir: TTwoBridgeConnectionDirection; player: TPlayer): TTwoBridgeConnectionInformation;
+      function  PointIsSideConnection(X, Y: shortint; player: TPlayer): boolean;
+      function  GetSideConnectionCarrier(X, Y: shortint; dir: TSideConnectionDirection): TSideConnectionCarrier;
+      function  GetSideConnectionInfo(X, Y: shortint; dir: TSideConnectionDirection; player: TPlayer): TSideConnectionInformation;
     public
       function  FindMove(const board: TGameBoard; player: TPlayer): TPoint;
       class constructor Create();
@@ -71,11 +85,11 @@ var
                   (count: 2; arr: @FirstMoves7)
                 );
 
-  Template_TwoBridgeConnectionsPoints: packed array [TBCDTopLeft..TBCDBottomLeft] of TRelativePoint = (
+  Template_TwoBridgeConnectionsPoints: packed array [CDTopLeft..CDBottomLeft] of TRelativePoint = (
                               (X: -1; Y: -1), (X: 1;  Y: -2), (X: 2;  Y: -1),
                               (X: 1;  Y: 1),  (X: -1; Y: 2),  (X: -2; Y: 1)
                             );
-  Template_TwoBridgeConnectionsCarriers: packed array [TBCDTopLeft..TBCDBottomLeft] of TTwoBridgeConnectionCarrier = (
+  Template_ConnectionsCarriers: packed array [CDTopLeft..CDBottomLeft] of TTwoBridgeConnectionCarrier = (
                               ((X: -1; Y: 0),  (X: 0;  Y: -1)),
                               ((X: 0;  Y: -1), (X: 1;  Y: -1)),
                               ((X: 1;  Y: -1), (X: 1;  Y: 0)),
@@ -84,12 +98,73 @@ var
                               ((X: -1; Y: 1),  (X: -1; Y: 0))
                             );
 
+
+
+function  TAIDefault.GetSideConnectionInfo(X, Y: shortint; dir: TSideConnectionDirection; player: TPlayer): TSideConnectionInformation;
+var
+  SrcCell:        TCell;
+  CarrierCells:   array [0..1] of TCell;
+  carrier:        TSideConnectionCarrier;
+  OppositePlayer: TPlayer;
+begin
+  carrier := Self.GetSideConnectionCarrier(X, Y, dir);
+
+  result.direction := dir;
+  result.carrier := carrier;
+
+  if player = PlayerNone then
+    begin
+      result.state := SCSUnknown;
+      exit();
+    end;
+  result.state := SCSInvalid;
+
+  if Self.Fboard.CheckBounds(carrier[0].X, carrier[0].Y) and
+     Self.Fboard.CheckBounds(carrier[1].X, carrier[1].Y) then
+    begin
+      if not (Self.PointIsSideConnection(carrier[0].X, carrier[0].Y, player) and
+              Self.PointIsSideConnection(carrier[1].X, carrier[1].Y, player)) then
+        exit();
+
+      SrcCell := Self.Fboard.GetCell(X, Y);
+      CarrierCells[0] := Self.Fboard.GetCell(carrier[0].X, carrier[0].Y);
+      CarrierCells[1] := Self.Fboard.GetCell(carrier[1].X, carrier[1].Y);
+
+      OppositePlayer := Self.GetOppositePlayer(player);
+
+      if SrcCell.Player = player then
+        begin
+          if (CarrierCells[0].Player = player) or (CarrierCells[1].Player = player) then
+            result.state := SCSRealized
+          else if (CarrierCells[0].Player = OppositePlayer) and (CarrierCells[1].Player = PlayerNone) then
+            begin
+              result.state := SCSDangerous;
+              result.PriorityCell := CarrierCells[1];
+            end
+          else if (CarrierCells[1].Player = OppositePlayer) and (CarrierCells[0].Player = PlayerNone) then
+            begin
+              result.state := SCSDangerous;
+              result.PriorityCell := CarrierCells[0];
+            end
+          else if (CarrierCells[0].Player = PlayerNone) and (CarrierCells[1].Player = PlayerNone) then
+            result.state := SCSSafe;
+        end
+      else if SrcCell.Player = PlayerNone then
+        if (CarrierCells[0].Player = PlayerNone) and (CarrierCells[1].Player = PlayerNone) then
+          result.state := SCSFree
+        else if (CarrierCells[0].Player = player) and (CarrierCells[1].Player = PlayerNone) then
+          result.state := SCSSafe
+        else if (CarrierCells[1].Player = player) and (CarrierCells[0].Player = PlayerNone) then
+          result.state := SCSSafe;
+    end;
+end;
+
 function  TAIDefault.GetTwoBridgeConnectionInfo(X, Y: shortint; dir: TTwoBridgeConnectionDirection; player: TPlayer): TTwoBridgeConnectionInformation;
 var
   point:   TPointRecord;
   DstCell, SrcCell: TCell;
-  carrier: TTwoBridgeConnectionCarrier;
   CarrierCells: array [0..1] of TCell;
+  carrier: TTwoBridgeConnectionCarrier;
   CarrierIsFree: boolean;
   OppositePlayer: TPlayer;
 begin
@@ -100,7 +175,7 @@ begin
   result.point := point;
   result.carrier := carrier;
   result.state := TBCSInvalid;
-  result.BoardConnection := false;
+  result.SideConnection := false;
 
   if player = PlayerNone then
     begin
@@ -117,10 +192,7 @@ begin
       CarrierCells[0] := Self.Fboard.GetCell(carrier[0].X, carrier[0].Y);
       CarrierCells[1] := Self.Fboard.GetCell(carrier[1].X, carrier[1].Y);
 
-      if player = PlayerOne then
-        OppositePlayer := PlayerTwo
-      else
-        OppositePlayer := PlayerOne;
+      OppositePlayer := Self.GetOppositePlayer(player);
 
       CarrierIsFree := false;
       if (CarrierCells[0].Player = PlayerNone) and (CarrierCells[1].Player = PlayerNone) then
@@ -156,15 +228,33 @@ begin
       else if (DstCell.Player = PlayerNone) and (SrcCell.Player = PlayerNone) and CarrierIsFree then
         result.state := TBCSFree;
 
-      if player = PlayerOne then
-        begin
-          if (SrcCell.Y in [0, Self.Fboard.MaxIndex]) or (DstCell.Y in [0, Self.Fboard.MaxIndex]) then
-            result.BoardConnection := true;
-        end
-      else if player = PlayerTwo then
-        if (SrcCell.X in [0, Self.Fboard.MaxIndex]) or (DstCell.X in [0, Self.Fboard.MaxIndex]) then
-          result.BoardConnection := true;
+      result.SideConnection := PointIsSideConnection(DstCell.X, DstCell.Y, player);
+      if not result.SideConnection then
+        result.SideConnection := PointIsSideConnection(SrcCell.X, SrcCell.Y, player);
     end;
+end;
+
+function  TAIDefault.PointIsSideConnection(X, Y: shortint; player: TPlayer): boolean;
+begin
+  result := false;
+  if player = PlayerOne then
+    begin
+      if Y in [0, Self.Fboard.MaxIndex] then
+        result := true;
+    end
+  else if player = PlayerTwo then
+    if X in [0, Self.Fboard.MaxIndex] then
+      result := true;
+end;
+
+function  TAIDefault.GetOppositePlayer(player: TPlayer): TPlayer;
+begin
+  if player = PlayerOne then
+    result := PlayerTwo
+  else if player = PlayerTwo then
+    result := PlayerOne
+  else
+    result := PlayerNone;
 end;
 
 function  TAIDefault.GetPointByRelative(X, Y: shortint; var pt: TRelativePoint): TPointRecord;
@@ -182,9 +272,14 @@ function  TAIDefault.GetTwoBridgeConnectionCarrier(X, Y: shortint; dir: TTwoBrid
 var
   carrier: TTwoBridgeConnectionCarrier;
 begin
-  carrier := Template_TwoBridgeConnectionsCarriers[dir];
+  carrier := Template_ConnectionsCarriers[dir];
   result[0] := Self.GetPointByRelative(X, Y, carrier[0]);
   result[1] := Self.GetPointByRelative(X, Y, carrier[1]);
+end;
+
+function  TAIDefault.GetSideConnectionCarrier(X, Y: shortint; dir: TSideConnectionDirection): TSideConnectionCarrier;
+begin
+  result := Self.GetTwoBridgeConnectionCarrier(X, Y, dir);
 end;
 
 function  TAIDefault.GetFirstMove(BoardSide: shortint): TPoint;
@@ -209,6 +304,7 @@ var
   move:   TMove;
   dir:    TTwoBridgeConnectionDirection;
   tbinfo: TTwoBridgeConnectionInformation;
+  sinfo:  TSideConnectionInformation;
 begin
   result := nil;
   if board.BoardSide > 7 then
@@ -223,16 +319,28 @@ begin
         begin
         arr[move.Cell.X, move.Cell.Y] := 100;
         if move.Cell.Player = player then
-          for dir in [TBCDTopLeft..TBCDBottomLeft] do
-            begin
-            tbinfo := Self.GetTwoBridgeConnectionInfo(move.Cell.X, move.Cell.Y, dir, player);
-            Write(tbinfo.direction, '; ', tbinfo.state);
-            if (tbinfo.state = TBCSNotRealized) or (tbinfo.state = TBCSDangerous) then
-              Write('; priority: (', tbinfo.PriorityCell.X, ', ', tbinfo.PriorityCell.Y, ')');
-            if tbinfo.BoardConnection then
-              Write('; board_conn');
-            WriteLn();
-            end;
+          begin
+            WriteLn('--------- TwoBridge:');
+            for dir in [CDTopLeft..CDBottomLeft] do
+              begin
+              tbinfo := Self.GetTwoBridgeConnectionInfo(move.Cell.X, move.Cell.Y, dir, player);
+              Write(tbinfo.direction, '; ', tbinfo.state);
+              if (tbinfo.state = TBCSNotRealized) or (tbinfo.state = TBCSDangerous) then
+                Write('; priority: (', tbinfo.PriorityCell.X, ', ', tbinfo.PriorityCell.Y, ')');
+              if tbinfo.SideConnection then
+                Write('; side_conn');
+              WriteLn();
+              end;
+            WriteLn('--------- Side:');
+            for dir in [CDTopLeft..CDBottomLeft] do
+              begin
+              sinfo := Self.GetSideConnectionInfo(move.Cell.X, move.Cell.Y, dir, player);
+              Write(sinfo.direction, '; ', sinfo.state);
+              if sinfo.state = SCSDangerous then
+                Write('; priority: (', sinfo.PriorityCell.X, ', ', sinfo.PriorityCell.Y, ')');
+              WriteLn();
+              end;
+          end;
         end;
     end;
 end;
